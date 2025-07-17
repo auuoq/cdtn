5dbcb1408e8c2e3cd291204a05696c99a7cfee74
auuoq <datbon1810@gmail.com>
2025-07-17 10:22:31 +0700

    55555...5

--- nodejs/src/config/connectDB.js
+++ nodejs/src/config/connectDB.js
@@ -11,6 +11,9 @@ const sequelize = new Sequelize(
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         dialect: 'postgres',
+        dialectOptions: {
+            family: 4 // Force IPv4
+        },
         logging: false,
         dialectOptions:
             process.env.DB_SSL === 'true' ?
