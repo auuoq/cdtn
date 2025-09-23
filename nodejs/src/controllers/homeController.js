9a377f2ae28465dc64570d13b1c873ab7e6a99f7
auuoq <datbon1810@gmail.com>
2025-09-23 14:42:51 +0700

    dp fe

--- nodejs/src/controllers/homeController.js
+++ nodejs/src/controllers/homeController.js
@@ -4,9 +4,6 @@ import CRUDService from "../services/CRUDService";
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
-        console.log('-----------------------');
-        console.log(data);
-        console.log('-----------------------');
         return res.render('homepage.ejs', {
             data: JSON.stringify(data)
         });
